ee2b143cb7424583f0863183ff467357e8475add
Jeremy Kurian <jeremykurian68@gmail.com>
2023-04-09 19:48:00 -0400

    slight progress of css

--- kwcsite/src/pages/pastlessons/index.js
+++ kwcsite/src/pages/pastlessons/index.js
@@ -13,8 +13,9 @@ export async function getStaticProps() {
 export default function PastLessons({ pastLessons }) {
   return (
     <div>
-      <h1>Past Lessons</h1>
+      <h1 className="text-white text-4xl">Past Lessons</h1>
       <ul>
+        <hr className="bg-white text-white w-full h-1"></hr>
         {pastLessons.map((lesson) => (
           <div key={lesson.id}>
             <PresentationShortView
@@ -26,9 +27,10 @@ export default function PastLessons({ pastLessons }) {
             />
             <Link href={`/pastlessons/${lesson.id}`}>
               <button className=" text-white transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">
-                View Lesson Details
+                View Lesson
               </button>
             </Link>
+            <hr className="bg-white text-white w-full h-1"></hr>
           </div>
         ))}
       </ul>
