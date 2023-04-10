4097a9fe8059e677de6c65a92574cf243d6860ed
Jeremy Kurian <jeremykurian68@gmail.com>
2023-04-09 20:08:37 -0400

    better styling on presentations w/ button pop

--- kwcsite/components/PresentationShortView.js
+++ kwcsite/components/PresentationShortView.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Link from "next/link";
 function PresentationShortView({ title, date, creator, id, slidesURL }) {
   return (
     <div>
@@ -14,6 +14,12 @@ function PresentationShortView({ title, date, creator, id, slidesURL }) {
           height="280"
         ></iframe>
       </li>
+      <Link href={`/pastlessons/${id}`}>
+        <button className="bg-white text-blue-900 py-2 px-4 rounded-full font-bold text-lg  transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+          View Lesson
+        </button>
+      </Link>
+      <hr className="bg-white my-8" />
     </div>
   );
 }
