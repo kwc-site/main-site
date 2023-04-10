f51bd020b3b5248c4117295ba0b5d37f530239a3
Ari Kamat <arijkamat@gmail.com>
2023-04-09 23:14:08 -0400

    add font awesome

--- kwcsite/components/Footer.js
+++ kwcsite/components/Footer.js
@@ -2,10 +2,13 @@ import React from "react";
 import Link from "next/link";
 import { Router, useRouter } from "next/router";
 import Image from "next/image";
-
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faInstagram,faDiscord} from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <footer className="bg-blue-950 mt-auto bottom-0 left-0 w-full text-center">
+
       <nav className="mx-auto">
         <ul className="flex justify-center mt-2 mb-3 text-lg">
           <li className="mr-6 ml-6 text-white flex items-center">
@@ -13,28 +16,13 @@ function Footer() {
               href="https://instagram.com/njitkidswhocode?igshid=YmMyMTA2M2Y="
               className="mr-10"
             >
-              <Image
-                src="/clubphotos/instagram.png"
-                width="50"
-                height="50"
-                alt="Instagram logo"
-              />
+              <FontAwesomeIcon icon ={faInstagram}></FontAwesomeIcon>
             </a>
             <a href="https://discord.gg/jKDhSJBWEe" className="mt-1 mr-10">
-              <Image
-                src="/clubphotos/discordlogo.png"
-                width="45"
-                height="45"
-                alt="Discord logo"
-              />
+              <FontAwesomeIcon icon={faDiscord}/>
             </a>
             <a href="mailto: njitkwc@gmail.com" className="mt-1">
-              <Image
-                src="/clubphotos/emaillogo.png"
-                height="80"
-                width="63"
-                alt="Discord logo"
-              />
+            <FontAwesomeIcon icon ={faEnvelope}></FontAwesomeIcon>
             </a>
           </li>
         </ul>
