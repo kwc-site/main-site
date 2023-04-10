c7ff4a2aa0e03ee8ea2c9958b372982db416acfc
Jeremy Kurian <jeremykurian68@gmail.com>
2023-04-09 20:21:41 -0400

    better formatted with flexbox

--- kwcsite/src/pages/pastlessons/index.js
+++ kwcsite/src/pages/pastlessons/index.js
@@ -14,7 +14,7 @@ export default function PastLessons({ pastLessons }) {
   return (
     <div className="w-full text-white min-h-screen">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-8">Past Lessons</h1>
+        <h1 className="text-5xl font-bold mb-8">Past Lessons</h1>
         <hr className="bg-white my-8" />
         <ul>
           {pastLessons.map((lesson) => (
