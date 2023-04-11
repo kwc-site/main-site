9b93ad1aafa1b39b100de6f553e34542ed65b500
Jeremy Kurian <jeremykurian68@gmail.com>
2023-04-11 14:06:11 -0400

    kinda nice homepage animation

--- kwcsite/src/pages/index.js
+++ kwcsite/src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
   return (
@@ -12,18 +10,36 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>
-          <div className="mt-10 transition-all">
-            <h1 className="text-9xl text-white text-right mr-12 pr-28 mt-10">
-              NJIT
-            </h1>
-            <h1 className="text-7xl text-white text-right mr-4">
-              Kids Who Code
-            </h1>
+      <main className="w-full flex flex-row text-white mt-5">
+        <div className="w-1/2 p-10">
+          <div className="border-2 border-gray-700 rounded-lg p-6 text-6xl">
+            <Typewriter
+              options={{
+                strings: "NJIT Kids Who Code",
+                autoStart: true,
+                loop: false,
+                deleteSpeed: 20,
+                delay: 50,
+                pauseFor: 2000,
+              }}
+              style={{ fontSize: "5rem", textAlign: "left" }}
+            />
           </div>
         </div>
+        <div className="flex-1 p-10 text-xl text-right mt-3">
+          <p>
+            A professional club at New Jersey Institute of Technology dedicated
+            to teaching programming to young children through a unique
+            curriculum containing Scratch.
+          </p>
+        </div>
       </main>
+      <style jsx>{`
+        p {
+          font-size: 1.5rem;
+          line-height: 1.5;
+        }
+      `}</style>
     </>
   );
 }
