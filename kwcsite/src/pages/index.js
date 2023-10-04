584cbc7f6e6422b05d26faca9073c0df28d9a638
Ari Kamat <arijkamat@gmail.com>
2023-10-04 14:52:09 -0400

    Modified index.js

--- kwcsite/src/pages/index.js
+++ kwcsite/src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>KidsWhoCode</title>
+        <title>Kids Who Code</title>
         <meta name="description" content="Kids Who Code Club Site" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
